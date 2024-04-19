0a4af58f4edd4709a5216fdb69cd9b8b211106d2
Najam Ul Saqib <najamulsaqib@tutamail.com>
2024-04-19 13:35:34 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { storage } from "./firebase";
+import {
+  ref,
+  getDownloadURL,
+  uploadBytesResumable,
+  listAll,
+  deleteObject,
+} from "firebase/storage";
 
 function App() {
+  const [imgUrl, setImgUrl] = useState(null);
+  const [progressPercent, setProgressPercent] = useState(0);
+  const [deleteFileName, setDeleteFileName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const file = e.target[0]?.files[0];
+    if (!file) return;
+    const storageRef = ref(storage, `files/${file.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+        setProgressPercent(progress);
+      },
+      (error) => {
+        alert(error);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImgUrl(downloadURL);
+        });
+      }
+    );
+  };
+
+  const handleDownloadAllContent = () => {
+    const storageRef = ref(storage, "files/");
+
+    listAll(storageRef)
+      .then((res) => {
+        res.items.forEach((itemRef) => {
+          getDownloadURL(itemRef)
+            .then((downloadURL) => {
+              console.log("Downloading:", downloadURL);
+            })
+            .catch((error) => {
+              console.error("Error getting download URL:", error);
+            });
+        });
+      })
+      .catch((error) => {
+        console.error("Error listing items:", error);
+      });
+  };
+
+  const handleDeleteFile = () => {
+    const fileRef = ref(storage, `files/${deleteFileName}`);
+    deleteObject(fileRef)
+      .then(() => {
+        console.log(`${deleteFileName} deleted successfully`);
+        setDeleteFileName("");
+      })
+      .catch((error) => {
+        console.error("Error deleting file:", error);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit} className="form">
+        <input type="file" />
+        <button type="submit">Upload</button>
+      </form>
+      <button onClick={handleDownloadAllContent}>Download All Content</button>
+      <div className="delete-file">
+        <input
+          type="text"
+          value={deleteFileName}
+          onChange={(e) => setDeleteFileName(e.target.value)}
+          placeholder="Enter filename to delete"
+        />
+        <button onClick={handleDeleteFile}>Delete File</button>
+      </div>
+      {!imgUrl && (
+        <div className="progress-bar">
+          <div className="inner-bar" style={{ width: `${progressPercent}%` }}>
+            {progressPercent}%
+          </div>
+        </div>
+      )}
+      {imgUrl && <img src={imgUrl} alt="uploaded file" height={200} />}
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "",
+  projectId: "",
+  appId: "",
+  storageBucket: "",
+  locationId: "",
+  authDomain: "",
+  messagingSenderId: "",
+  measurementId: "",
+};
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
